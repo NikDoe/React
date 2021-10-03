5d605c0b051693c93bf4488d0fa0d395d51122aa
NikDoe <nikd0e@yandex.ru>
2021-10-03 12:17:59 +0300

    feature (my-app) : create custom component

---
+++ my-app/src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>October 3th 2021</div>
+            <div>
+                <h2>Car insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
