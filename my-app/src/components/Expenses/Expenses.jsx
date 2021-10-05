52d2a2c61788c01cad16a4534ce167291d8044e7
NikDoe <nikd0e@yandex.ru>
2021-10-05 12:58:26 +0300

    feature (my-app) : output conditional content

--- my-app/src/components/Expenses/Expenses.jsx
+++ my-app/src/components/Expenses/Expenses.jsx
@@ -14,17 +14,26 @@ function Expenses(props) {
 
     const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
             <Card className="expenses">
-                <ExpensesFilter className='expenses-filter' selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {filteredExpenses.map(expense =>
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                )}
+                <ExpensesFilter
+                    className='expenses-filter'
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}/>
+                {expensesContent}
             </Card>
     );
 }
