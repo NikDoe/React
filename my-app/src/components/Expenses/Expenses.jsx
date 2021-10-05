c474055e9101e00475efbff922eca5ddddf0541a
NikDoe <nikd0e@yandex.ru>
2021-10-05 13:14:53 +0300

    refactor (my-app) : output conditional content

--- my-app/src/components/Expenses/Expenses.jsx
+++ my-app/src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
 
 
 function Expenses(props) {
@@ -14,26 +14,13 @@ function Expenses(props) {
 
     const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
-
     return (
             <Card className="expenses">
                 <ExpensesFilter
                     className='expenses-filter'
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}/>
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
     );
 }
