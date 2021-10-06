7368ad8a3488f7a6f60163fd5be19fb39a5ea79b
NikDoe <nikd0e@yandex.ru>
2021-10-06 18:29:39 +0300

    feature (goal-list): use CSS modules

--- goal-list/src/components/UI/Button/Button.js
+++ goal-list/src/components/UI/Button/Button.js
@@ -1,30 +1,11 @@
-import styled from 'styled-components';
+import styles from './Button.module.css'
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #1f1f1f;
-  border-radius: 5px;
-  color: white;
-  background: #222222;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  transition: background .3s ease-in-out;
-
-  @media (min-width: 768px) {
-    width: auto;
-  }
-  
-  &:focus {
-    outline: none;
-  }
-  &:hover,
-  &:active {
-    background: #353535;
-    border-color: #222222;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+const Button = props => {
+    return (
+        <button type={props.type} className={styles.button} onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
+};
 
 export default Button;
