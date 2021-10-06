feb5b27f89cb4c3c9aa2c4f8040bebc6b2c60710
NikDoe <nikd0e@yandex.ru>
2021-10-06 16:07:47 +0300

    feature (goal-list): use styled components

--- goal-list/src/components/UI/Button/Button.js
+++ goal-list/src/components/UI/Button/Button.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
-
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #1f1f1f;
+  border-radius: 5px;
+  color: white;
+  background: #222222;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  transition: background .3s ease-in-out;
+  &:focus {
+    outline: none;
+  }
+  &:hover,
+  &:active {
+    background: #353535;
+    border-color: #222222;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
