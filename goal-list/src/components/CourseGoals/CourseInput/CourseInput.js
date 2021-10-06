9d2211757549f4ad23c54fdb2b1289c44d7b5739
NikDoe <nikd0e@yandex.ru>
2021-10-06 15:39:31 +0300

    feature (goal-list) : dynamic inline styles

--- goal-list/src/components/CourseGoals/CourseInput/CourseInput.js
+++ goal-list/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,24 +5,39 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-control">
+          <label style={{ color: !isValid ? 'rgba(255,0,0,0.57)' : '#222222' }}>Course Goal</label>
+          <input
+              style={{
+                borderColor: !isValid ? 'rgba(255,0,0,0.57)' : '#ccc',
+                background: !isValid ? 'rgba(255,146,146,0.81)' : 'transparent'
+              }}
+              type="text"
+              onChange={goalInputChangeHandler}
+          />
+        </div>
+        <Button type="submit">Add Goal</Button>
+      </form>
   );
 };
 
