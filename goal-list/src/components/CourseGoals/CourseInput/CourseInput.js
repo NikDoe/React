44cd48c321667de87e021a92f4e02acd836c7136
NikDoe <nikd0e@yandex.ru>
2021-10-06 18:40:15 +0300

    refactor (goal-list): use CSS modules

--- goal-list/src/components/CourseGoals/CourseInput/CourseInput.js
+++ goal-list/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'rgba(255,0,0,0.57)' : '#222222')};
-  }
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'rgba(255,0,0,0.57)' : '#222222')};
-    background: ${props => (props.invalid ? 'rgba(255,146,146,0.81)' : 'transparent')};
-    border-radius: 5px;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    color: #818181;
-    transition: all .3s ease-in-out;
-  }
-  & input:focus {
-    outline: none;
-    background: rgba(255, 255, 36, 0.4);
-    color: #111111;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -54,10 +26,12 @@ const CourseInput = props => {
 
   return (
       <form onSubmit={formSubmitHandler}>
-        <FormControl invalid={!isValid}>
+        <div
+            className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+        >
           <label>Course Goal</label>
           <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
-        </FormControl>
+        </div>
         <Button type="submit">Add Goal</Button>
       </form>
   );
