1202576c416206bc7e0dbb83a5a73143226fd3a6
NikDoe <nikd0e@yandex.ru>
2021-10-07 12:46:32 +0300

    feature(s-u-v): manage inputs state

--- simple-user-validation/src/components/Users/AddUser.jsx
+++ simple-user-validation/src/components/Users/AddUser.jsx
@@ -1,20 +1,40 @@
 import Card from "../UI/Card";
 import styles from './AddUser.module.css';
 import Button from "../UI/Button";
+import {useState} from "react";
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = event => {
         event.preventDefault();
-        console.log('add new user');
+        console.log(enteredUsername, enteredAge);
     }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
 
     return(
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">your name</label>
-                <input type="text" id='username'/>
-                <label htmlFor="age">your age</label>
-                <input type="text" id='age'/>
+                <input
+                    type="text"
+                    id='username'
+                    onChange={usernameChangeHandler}
+                />
+                <label htmlFor="age">your age(years)</label>
+                <input
+                    type="number"
+                    id='age'
+                    onChange={ageChangeHandler}
+                />
                 <Button type='submit'>add user</Button>
             </form>
         </Card>
