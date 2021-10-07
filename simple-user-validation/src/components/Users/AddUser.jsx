93e08476ab6116a1ce9671d03b32e405e62bfa7d
NikDoe <nikd0e@yandex.ru>
2021-10-07 12:23:12 +0300

    feature(s-u-v): add re-usable card component

--- simple-user-validation/src/components/Users/AddUser.jsx
+++ simple-user-validation/src/components/Users/AddUser.jsx
@@ -1,3 +1,6 @@
+import Card from "../UI/Card";
+import styles from './AddUser.module.css';
+
 const AddUser = props => {
     const addUserHandler = event => {
         event.preventDefault();
@@ -5,13 +8,15 @@ const AddUser = props => {
     }
 
     return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">your name</label>
-            <input type="text" id='username'/>
-            <label htmlFor="age">your age</label>
-            <input type="text" id='age'/>
-            <button type='submit'>add</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">your name</label>
+                <input type="text" id='username'/>
+                <label htmlFor="age">your age</label>
+                <input type="text" id='age'/>
+                <button type='submit'>add</button>
+            </form>
+        </Card>
     )
 }
 
