b2cfc8b74b3eea17929ca042be9cd3aa2dcc99bb
NikDoe <nikd0e@yandex.ru>
2021-10-07 15:26:41 +0300

    feature(s-u-v): manage errors state

--- simple-user-validation/src/components/UI/ErrorModal.jsx
+++ simple-user-validation/src/components/UI/ErrorModal.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 const ErrorModal = props => {
     return (
         <div>
-            <div className={styles.backdrop} />
+            <div className={styles.backdrop} onClick={props.onConfirm}/>
             <Card className={styles.modal}>
                 <header className={styles.header}>
                     <h2>{props.title}</h2>
@@ -14,7 +14,7 @@ const ErrorModal = props => {
                     <p>{props.message}</p>
                 </div>
                 <footer className={styles.actions}>
-                    <Button>okay</Button>
+                    <Button onClick={props.onConfirm}>okay</Button>
                 </footer>
             </Card>
         </div>
