474da2e28d7aca45e554f94ecd62547209cf9e65
NikDoe <nikd0e@yandex.ru>
2021-10-07 13:44:16 +0300

    feature(s-u-v): manage UsersList state

--- simple-user-validation/src/App.js
+++ simple-user-validation/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 
 function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addNewUserHandler = (userName, userAge) => {
+        setUsersList(
+            prevUsersList => {
+                return [
+                    ...prevUsersList,
+                    {
+                        name: userName,
+                        age: userAge,
+                        id: new Date().getTime()
+                    }
+                ]
+            }
+        )
+    }
     return (
         <div>
-            <AddUser/>
-            <UsersList users={[]}/>
+            <AddUser onAddUser={addNewUserHandler}/>
+            <UsersList users={usersList}/>
         </div>
     )
 }
