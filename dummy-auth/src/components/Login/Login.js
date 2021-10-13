d66c24a1bf65fceef705e6e677a0d546ad0b5208
NikDoe <nikd0e@yandex.ru>
2021-10-13 19:20:26 +0300

    refactor(auth): useReducer with useEffect

--- dummy-auth/src/components/Login/Login.js
+++ dummy-auth/src/components/Login/Login.js
@@ -41,11 +41,14 @@ const Login = (props) => {
       {value : '', isValid: null}
   );
 
-  /*useEffect(() => {
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
-          enteredEmail.includes('@') && enteredPassword.trim().length > 6
+          emailIsValid && passwordIsValid
       );
     }, 500);
 
@@ -53,21 +56,15 @@ const Login = (props) => {
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]);*/
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     // setEnteredEmail(event.target.value);
     dispatchEmail({type: 'USER_INPUT', value: event.target.value})
-    setFormIsValid(
-        event.target.value.includes('@') && passwordState.isValid
-    );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_INPUT', value: event.target.value});
-    setFormIsValid(
-        emailState.isValid && event.target.value.trim().length > 6
-    );
   };
 
   const validateEmailHandler = () => {
